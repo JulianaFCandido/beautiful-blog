bb1e047827f8cf5407cfcb05c0954bca7c3924cc
juliana.candido <jufcandido@hotmail.com>
2024-10-31 00:31:52 -0300

    Add rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root to: 'home#show'
+
+  resources :articles
+  resources :comments
+  resources :categories
+  resource :contact, only: %i[new create], controller: :contact
+  resource :about, only: %i[show], controller: :about
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -8,15 +17,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-  root to: 'home#show'
-
-  devise_for :users
-
-  resources :comments
-  resources :articles
-  resources :categories
 end
