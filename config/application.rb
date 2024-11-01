8eee475a106ef9dd921e2e7ddad0138700bf3daf
juliana.candido <jufcandido@hotmail.com>
2024-11-01 14:50:30 -0300

    Update application config

--- config/application.rb
+++ config/application.rb
@@ -13,17 +13,15 @@ module BeautifulBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    config.time_zone = 'America/Sao_Paulo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
+    config.i18n.default_locale = :'pt-BR'
   end
 end
